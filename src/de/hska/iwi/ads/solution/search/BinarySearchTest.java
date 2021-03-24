f9baf1569d569309df551c386d6d1054998efa9a
Stephanie.Thiele <stephanie.thiele-bergmannmx.de>
2021-03-24 11:27:50 +0100

    Nochmal minimale Änderungen

--- src/de/hska/iwi/ads/solution/search/BinarySearchTest.java
+++ src/de/hska/iwi/ads/solution/search/BinarySearchTest.java
@@ -47,15 +47,7 @@ public class BinarySearchTest extends SearchTest {
 		assertEquals(1, search.search(a, 1, 3, 0));
 	}
 
-	/*
-	 * I tried :) Ich bin tatsächlich beim gleichen Code rausgekommen. Ich habe
-	 * verschiedenes probiert, aber nur das, was du schon hattest, ging. Ich habe
-	 * unten eine Frage zum Code an sich einkommentiert, damit ich die nicht
-	 * vergesse. Außerdem ToDo: Die ganzen Kommentare entfernen vor der Abgabe!
-	 */
-
-	// key is larger than middle element - passt das so? Das testet doch eigentlich
-	// nur für die 6....
+	// key is larger than middle element
 	@Test
 	void testBinarySearchLargerThanMiddle() {
 		Search<Integer> search = createSearch();
@@ -80,7 +72,6 @@ public class BinarySearchTest extends SearchTest {
 	}
 
 	// not found
-	@Ignore
 	@Test
 	void testBinarySearchNotFound() {
 		Search<Integer> search = createSearch();
@@ -88,15 +79,14 @@ public class BinarySearchTest extends SearchTest {
 		assertEquals(5, search.search(a, 6));
 	}
 
-	// anywhere in array
+	// anywhere in array - reuse search
 	@Test
-	void testBinarySearchAnyhere() {
+	void testBinarySearchReuseSearch() {
 		Search<Integer> search = createSearch();
-		Integer[] a = { 1, 2, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10 };
-		assertEquals(7, search.search(a, 5));
-		// geht das?
-		Integer[] b = { 1, 2, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10 };
-		assertEquals(9, search.search(b, 7));
+		Integer[] a = { 0, 1, 2, 3, 4 };
+		assertEquals(0, search.search(a, 0));
+		assertEquals(1, search.search(a, 1));
+		//
 	}
 
 	// first in left, right, middle
@@ -137,10 +127,9 @@ public class BinarySearchTest extends SearchTest {
 	}
 
 	// in first half
-	// key < a[left] nicht abgedeckt durch in first half???
 	@Test
 	void testBinarySearchInFirstHalf() {
-		Search<Integer>/* Eclipse gefolgt, aber warum eigentlich? */ search = createSearch();
+		Search<Integer> search = createSearch();
 		Integer[] a = { 2, 3, 4, 5, 6, 7 };
 		assertEquals(0, search.search(a, 2));
 	}
