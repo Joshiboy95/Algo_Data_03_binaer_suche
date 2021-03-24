4e8e7aafd8e000bd60db6bb8d9d67e4aa0aa610e
Joshua Rosenberger <rosenbergerjoshua@web.de>
2021-03-24 10:43:46 +0100

    Added Mergesort and MergesortTest files

---
+++ src/de/hska/iwi/ads/solution/sorting/MergesortTest.java
@@ -0,0 +1,14 @@
+package de.hska.iwi.ads.solution.sorting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MergesortTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
