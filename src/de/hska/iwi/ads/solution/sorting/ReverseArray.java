e421cad6830100554872e1834f2f9b1eee53ff06
Joshua Rosenberger <rosenbergerjoshua@web.de>
2021-03-24 10:52:04 +0100

    added all class files for 4. task sheet

---
+++ src/de/hska/iwi/ads/solution/sorting/ReverseArray.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.ads.solution.sorting;
+
+import de.hska.iwi.ads.sorting.Reverse;
+
+public class ReverseArray<E extends Comparable<E>> implements Reverse<E> {
+
+	@Override
+	public void reverse(E[] a, int from, int to) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
