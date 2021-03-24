3b26992a4329055adad012879e930994bcea6fb0
Joshua Rosenberger <rosenbergerjoshua@web.de>
2021-03-24 15:20:16 +0100

    added MergesortBasis implementation

--- src/de/hska/iwi/ads/solution/sorting/MergesortBasis.java
+++ src/de/hska/iwi/ads/solution/sorting/MergesortBasis.java
@@ -1,11 +1,60 @@
 package de.hska.iwi.ads.solution.sorting;
 
-public class MergesortBasis extends de.hska.iwi.ads.sorting.AbstractMergesort{
-
+/**
+ * Sorts a given array using a merge sorting algorithm. 
+ * The entire array can be sorted or only a given range.
+ * 
+ * @author Joshua Rosenberger - rojo1041 - 68336
+ */
+public class MergesortBasis<E extends Comparable<E>> extends de.hska.iwi.ads.sorting.AbstractMergesort<E>{
+	E [] b;
+	
 	@Override
-	protected void mergesort(Comparable[] a, int left, int right) {
-		// TODO Auto-generated method stub
+	protected void mergesort(E [] a, int left, int right) {
 		
+		int middle;
+		
+		if(left < right) {
+			middle = (left + right) / 2;
+			mergesort(a, left , middle);
+			mergesort(a, middle+1, right);
+			merge(a, left, middle, right);
+		}
+		
+	}
+	
+	
+	private void merge(E [] a, int left, int middle, int right) {
+		
+		int l = left;
+		int r = middle +1;
+		
+		for (int i = left; i <= right; i++) {
+			if ((r > right) || 
+					((l <= middle) && (isLessOrEqual(a[l], a[r])))) {
+				b[i] = a[l];
+				l++;
+			} else {
+				b[i] = a[r];
+				r++;
+			}
+		}
+		
+		for (int i = left; i <= right; i++) {
+			a[i] = b[i];
+		}
+		
+	}
+	
+	/**
+	 * Compares two {@link Comparable} elements and returns if the first element is
+	 * less or equal than the second element.
+	 * @param firstElement of type {@link Comparable}
+	 * @param secondElement of type {@link Comparable}
+	 * @return true if the firstElement is less than the secondElement
+	 */
+	private boolean isLessOrEqual(E firstElement, E secondElement) {
+		return (firstElement.compareTo(secondElement) <= 0);
 	}
 
 }
