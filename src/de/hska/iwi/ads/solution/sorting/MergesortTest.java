dbbd6d35c83a284d1021061c7a05919c4a0ca742
Stephanie.Thiele <stephanie.thiele-bergmannmx.de>
2021-04-25 17:14:01 +0200

    Das war auch die falsche Datei

--- src/de/hska/iwi/ads/solution/sorting/MergesortTest.java
+++ src/de/hska/iwi/ads/solution/sorting/MergesortTest.java
@@ -6,116 +6,104 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import de.hska.iwi.ads.search.Search;
-import de.hska.iwi.ads.solution.search.BinarySearch;
+class MergesortTest {
 
-public class MergesortTest{
-	
-	//general test
+	// general test
 	@Test
 	void testMergesortGeneral() {
 		Mergesort<Integer> mergesort = new Mergesort<Integer>();
-			Integer[] a = { 2, 1, 7, 12, 5, 9, 8, 10 };
-			Integer[] result = { 1, 2, 5, 7, 8, 9, 10, 12 };
+		Integer[] a = { 2, 1, 7, 12, 5, 9, 8, 10 };
+		Integer[] result = { 1, 2, 5, 7, 8, 9, 10, 12 };
+		mergesort.sort(a);
+		assertArrayEquals(result, a);
+	}
+	
+	//empty list
+		@Test
+		void testMergesortIntArrayIntEmtpy() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = null;
+			Integer[] result = { };
+			mergesort.sort(a);
+			assertArrayEquals(result, a);
+		}
+		
+		// one element
+		@Test
+		void testMergesortOneElement() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = { 5 };
+			Integer[] result = { 5 };
+			mergesort.sort(a);
+			assertArrayEquals(result, a);
+		}	
+		
+		//two elements
+		@Test
+		void testMergesortTwoElements() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = { 5, 3 };
+			Integer[] result = { 3, 5 };
+			mergesort.sort(a);
+			assertArrayEquals(result, a);
+		}
+		
+		 //already sorted list
+		@Test
+		void testMergesortAlreadySortedList() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = { 1, 2, 3, 4, 5, 6};
+			Integer[] result = { 1, 2, 3, 4, 5, 6};
+			mergesort.sort(a);
+			assertArrayEquals(result, a);
+		}
+		
+		// reverse sorted list
+		@Test
+		void testMergesortReverseSortedList() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = {6, 5, 4, 3, 2, 1,};
+			Integer[] result = {1, 2, 3, 4, 5, 6,};
+			mergesort.sort(a);
+			assertArrayEquals(result, a);
+		}
+		
+		 // list all same element
+		@Test
+		void testMergesortAllSameElement() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = { 7, 7, 7, 7, 7};
+			Integer[] result = { 7, 7, 7, 7, 7};
+			mergesort.sort(a);
+			assertArrayEquals(result, a);
+		}
+		
+		 //array halves have even/odd number
+		@Test
+		void testMergesortArrayEvenNumber() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = { 5, 7, 6, 4, 8, 3, 1};
+			Integer[] result = { 1, 3, 4, 5, 6 ,7, 8};
+			mergesort.sort(a);
+			assertArrayEquals(result, a);
+		}
+		
+		@Test
+		void testMergesortArrayOddNumber() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = {5, 7, 6, 4, 8, 3, 1, 10};
+			Integer[] result = { 1, 3, 4, 5, 6 ,7, 8, 10};
+			mergesort.sort(a);
+			assertArrayEquals(result, a);
+		}
+		
+		 //duplicates
+		void testMergesortDuplicates() {
+			Mergesort<Integer> mergesort = new Mergesort<Integer>();
+			Integer[] a = {5, 7, 6, 4, 8, 3, 1, 10, 3, 8, 5,};
+			Integer[] result = { 1, 3, 3, 4, 5, 5, 6 ,7, 8, 8, 10};
 			mergesort.sort(a);
 			assertArrayEquals(result, a);
 		}
 	
-	//empty list
-	@Test
-	void testMergesortIntArrayIntEmtpy() {
-		Integer[] a = null;
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		assertThrows(NullPointerException.class, () -> ms.sort(a));
-	}
-	
-	// one element
-	@Test
-	void testMergesortOneElement() {
-		Integer[] a = { 5 };
-		Integer[] result = { 5 };
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		ms.sort(a);
-		assertTrue(/*?*/.equals(a, result));
-
-	}
-	
-	//two elements
-	@Test
-	void testMergesortTwoElements() {
-		Integer[] a = { 5, 3 };
-		Integer[] result = { 3, 5 };
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		ms.sort(a);
-		assertTrue(/*?*/.equals(a, result));
-
-	}
-	
-	 //already sorted list
-	@Test
-	void testMergesortAlreadySortedList() {
-		Integer[] a = { 1, 2, 3, 4, 5, 6};
-		Integer[] result = { 1, 2, 3, 4, 5, 6};
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		ms.sort(a);
-		assertTrue(/*?*/.equals(a, result));
-
-	}
-	
-	// reverse sorted list
-	@Test
-	void testMergesortReverseSortedList() {
-		Integer[] a = {6, 5, 4, 3, 2, 1,};
-		Integer[] result = {1, 2, 3, 4, 5, 6,};
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		ms.sort(a);
-		assertTrue(/*?*/.equals(a, result));
-
-	}
-	 // list all same element
-	@Test
-	void testMergesortAllSameElement() {
-		Integer[] a = { 7, 7, 7, 7, 7};
-		Integer[] result = { 7, 7, 7, 7, 7};
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		ms.sort(a);
-		assertTrue(/*?*/.equals(a, result));
-
-	}
-	
-	 //array halves have even/odd number
-	@Test
-	void testMergesortArrayEvenNumber() {
-		Integer[] a = { 5, 7, 6, 4, 8, 3, 1};
-		Integer[] result = { 1, 3, 4, 5, 6 ,7, 8};
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		ms.sort(a);
-		assertTrue(/*?*/.equals(a, result));
-
-	}
-	
-	@Test
-	void testMergesortArrayOddNumber() {
-		Integer[] a = {5, 7, 6, 4, 8, 3, 1, 10};
-		Integer[] result = { 1, 3, 4, 5, 6 ,7, 8, 10};
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		ms.sort(a);
-		assertTrue(/*?*/.equals(a, result));
-
-	}
-	
-	 //duplicates
-	void testMergesortDuplicates() {
-		Integer[] a = {5, 7, 6, 4, 8, 3, 1, 10, 3, 8, 5,};
-		Integer[] result = { 1, 3, 3, 4, 5, 5, 6 ,7, 8, 8, 10};
-		Mergesort<Integer> ms = new Mergesort<Integer>();
-		ms.sort(a);
-		assertTrue(/*?*/.equals(a, result));
-
-	}
-		
-		
-		
-	}
-
-
+}
