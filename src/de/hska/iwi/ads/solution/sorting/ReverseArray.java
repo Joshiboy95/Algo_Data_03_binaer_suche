f582ad28cfecf5fd54b026644d00755c3a43f958
Stephanie.Thiele <stephanie.thiele-bergmannmx.de>
2021-04-25 17:10:25 +0200

    Jetzt die richtige Datei

--- src/de/hska/iwi/ads/solution/sorting/ReverseArray.java
+++ src/de/hska/iwi/ads/solution/sorting/ReverseArray.java
@@ -4,45 +4,33 @@ import de.hska.iwi.ads.sorting.Reverse;
 
 public class ReverseArray<E extends Comparable<E>> implements Reverse<E> {
 
-	/*
-	 * don't want to make new array, switch first element with last element (use
-	 * indexes i), switch second element with second to last element and so on until
-	 * it reaches middle of array a
-	 * 
-	 * rev just a variable name
-	 */
-
 	
 	@Override
 	public void reverse(E[] a, int from, int to) {
 		
-		// reverse for complete array of integer
-		for (int i = 0; i < a.length /2; i++) {
-			// E is the type here
-			E rev = a[i];
-			a[i] = a[a.length - i - 1];
-			a[a.length - 1 - i] = rev;
+// in case from and to are switched
+		if(from > to) {
+			int f = 0;
+			f = from;
+			from  = to;
+			to = f;
+		}
 		
-
-		// reverse for left half
-		for (int j = 0; j < to; j++) {
-			E reve = a[j];
-			a[j] = a[from - j - 1];
-			a[to - 1 - j] = reve;
+		//(to-from)/2 to find middle
+		for (int i = 0; i < (to-from)/2; i++) {
+			//swapping out: first element (from) with last element(to), 
+			//second (from-i)with second-to-last(to-i)...
+			
+			//i+from to catch array from the very beginning
+			E reversed = a[i+from];
+			//to-1 to catch array from the very end
+			a[i+from] = a[to -i];
+			
+			a[to -i] = reversed;
+			
 		}
-
-		// reverse for right half
-		for (int k = 0; k > from; k++) {
-			E revex = a[k];
-			a[k] = a[from - k - 1];
-			a[to - 1 - k] = revex;
+		
 		}
-
-		/*
-		 * reverse for array of Strings reverse & for array of boolean: Sind die in der
-		 * Aufgabe überhaupt gefordert und wenn ja, müsste das nicht einfach mit String,
-		 * boolean statt int gehen?
-		 */
-	}
-	}
+	
+	
 }
