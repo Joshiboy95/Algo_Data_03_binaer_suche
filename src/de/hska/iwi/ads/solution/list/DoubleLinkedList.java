4ce1491382807d3f115516e332549f6a5916293c
Joshua Rosenberger <rosenbergerjoshua@web.de>
2021-05-10 22:39:22 +0200

    added DoubleLinkedList methods so i can add tests

--- src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
+++ src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
@@ -2,6 +2,19 @@ package de.hska.iwi.ads.solution.list;
 
 import de.hska.iwi.ads.dictionary.AbstractDoubleLinkedList;
 
-public class DoubleLinkedList extends AbstractDoubleLinkedList {
+public class DoubleLinkedList<K extends Comparable<K>, V> extends AbstractDoubleLinkedList<K, V> {
 
+	public V get(Object o) {
+		return null;
+	}
+	
+	public V put(K key, V value) {
+		return null;
+	}
+	
+	@Override
+	public V remove(Object key) {
+		// TODO Auto-generated method stub
+		return super.remove(key);
+	}
 }
