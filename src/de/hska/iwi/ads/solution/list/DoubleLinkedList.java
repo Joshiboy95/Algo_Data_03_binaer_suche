f2467b9ea548f9a72893e0a0b17dd35abe5f95d6
Joshua Rosenberger <rosenbergerjoshua@web.de>
2021-05-11 11:54:40 +0200

    chaged DoubleLinkedList methodes to be default auto-generated

--- src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
+++ src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
@@ -1,20 +1,26 @@
 package de.hska.iwi.ads.solution.list;
 
+import java.util.Map.Entry;
+
 import de.hska.iwi.ads.dictionary.AbstractDoubleLinkedList;
 
 public class DoubleLinkedList<K extends Comparable<K>, V> extends AbstractDoubleLinkedList<K, V> {
 
+	@Override
 	public V get(Object o) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 	
+	@Override
 	public V put(K key, V value) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 	
 	@Override
 	public V remove(Object key) {
 		// TODO Auto-generated method stub
-		return super.remove(key);
+		return null;
 	}
 }
