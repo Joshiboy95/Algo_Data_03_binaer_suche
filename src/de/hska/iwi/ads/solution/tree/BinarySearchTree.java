0f60fd728aa536adaf8eeceb104bf8fe4ea32e9e
Joshua Rosenberger <rosenbergerjoshua@web.de>
2021-05-11 11:22:05 +0200

    added empty BinarySearchTree methods to start creating tests

--- src/de/hska/iwi/ads/solution/tree/BinarySearchTree.java
+++ src/de/hska/iwi/ads/solution/tree/BinarySearchTree.java
@@ -1,5 +1,24 @@
 package de.hska.iwi.ads.solution.tree;
 
-public class BinarySearchTree {
+import de.hska.iwi.ads.dictionary.AbstractBinaryTree;
 
+public class BinarySearchTree<K extends Comparable<K>, V> extends AbstractBinaryTree<K, V> {
+	
+	@Override
+	public V get(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public V put(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public V remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
